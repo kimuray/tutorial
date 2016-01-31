a680283d0a5b0e7513cd2794451969d26810386c
Kimura Yoshihiro <kimura.yoshihiro@tsunolab.net>
2016-01-31 18:18:15 +0900

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -15,7 +15,13 @@ class StaticPagesControllerTest < ActionController::TestCase
 
   test "Should get about" do
     get :about
-	assert_response :success
+	  assert_response :success
     assert_select "title", "About|Ruby on Rails Tutorial Sample App"
+  end
+
+  test "Should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact|Ruby on Rails Tutorial Sample App"
   end
 end
