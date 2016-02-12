d55f853d18e521b3df16919fe5c7059192e146f8
Kimura Yoshihiro <kimura.yoshihiro@tsunolab.net>
2016-02-12 22:42:53 +0900

    Make abasic User model(including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
 	has_many :microposts
-	validates :name, presence: true
-	validates :email, presence: true
+	has_secure_password
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :email, presence: true, length: { maximum: 255 },
+		format: { with: VALID_EMAIL_REGEX }, 
+		uniqueness: { case_sensitive: false }
+	validates :password, presence: true, length: { minimum: 6 }
 end
